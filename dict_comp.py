04e47beb5c6e898dc53ff78d4ab27f246b512f65
kramstyles <mark.eke@decagon.dev>
2022-02-11 10:58:21 +0100

    dict_comp 90%

--- dict_comp.py
+++ dict_comp.py
@@ -1,2 +1,24 @@
 def dict_comp(stop, step):
-  pass
+    """
+    This function takes in two integer values 'stop' and 'step' and returns a
+    dictionary generated using python comprehension
+    :param stop: Int provided by user
+    :param step: Int provided by user
+    :return: bigArray : Dictionary of python comprehension
+    """
+    counter = 1
+    bigArray = {}
+    array = []
+    for num in range(1, stop + 1):
+        if len(array) < step:
+            array.append(num)
+        if len(array) == step:
+            bigArray[f"item_{counter}"] = array
+            array = []
+            counter += 1
+
+    return bigArray
+
+
+if __name__ == '__main__':
+    print(dict_comp(10, 4))
