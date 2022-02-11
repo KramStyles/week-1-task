77451fed98731d2d0a76172dc8d3b1c1e7fff466
kramstyles <mark.eke@decagon.dev>
2022-02-11 14:21:06 +0100

    working on cryptography 98%

--- cryptography/main.py
+++ cryptography/main.py
@@ -7,42 +7,45 @@ def cryptography():
     lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     repeat = 'Yes'
-    while repeat:
-      msg = ''
-      choice = input("Input 1 if you want to encrypt and 0 if you want to decrypt: ")
-      if choice == '0':
-        word = input('Enter word to be decoded: ')
-        level = int(input('Enter the key: '))
-        for x in word:
-          if x in lowers:
-            indx = lowers.index(x) - level
-            if indx < 0:
-              indx += len(lowers)
-            msg += lowers[indx]
-          else:
-            msg += x
-      elif choice == '1':
-        word = input('Enter word to be encoded: ')
-        level = int(input('Enter the key: '))
-        for x in word:
-          if x in lowers:
-            indx = lowers.index(x) + level
-            if indx > len(lowers):
-              indx -= len(lowers)
-            msg += lowers[indx]
-          else:
-            msg += x
-      else:
-        msg = 'Please, make a valid choice'
-      print(msg)
-      repeat = input('Do you want to use our crypt service again? ')
-      if repeat.title() == 'No':
-        print('Thank you for testing our product. We hope to see you soon!')
-        break;
-
-
-
-    return msg
+    try:
+        while repeat:
+            msg = ''
+            choice = input("Input 1 if you want to encrypt and 0 if you want to decrypt: ")
+            if choice == '0':
+                word = input('Enter ciphertext to be decoded: ')
+                level = int(input('Enter the key: '))
+                if level > len(lowers):
+                    level %= len(lowers)
+                for x in word:
+                    if x in lowers:
+                        indx = lowers.index(x) - level
+                        if indx < 0:
+                            indx += len(lowers)
+                        msg += lowers[indx]
+                    else:
+                        msg += x
+            elif choice == '1':
+                word = input('Enter plaintext to be encoded: ')
+                level = int(input('Enter the key: '))
+                if level > len(lowers):
+                    level %= len(lowers)
+                for x in word:
+                    if x in lowers:
+                        indx = lowers.index(x) + level
+                        if indx > len(lowers):
+                            indx -= len(lowers)
+                        msg += lowers[indx]
+                    else:
+                        msg += x
+            else:
+                msg = 'Please, make a valid choice'
+            print(msg)
+            repeat = input('Do you want to use our crypt service again? ')
+            if repeat.title() == 'No':
+                print('Thank you for testing our product. We hope to see you soon!')
+                break
+    except Exception as err:
+        print("An Error occurred:", err)
 
 
 cryptography()
