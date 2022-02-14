4d08af383cbdccc671c197ecdc6c7467bac10d55
kramstyles <kramstyles@outlook.com>
2022-02-14 21:24:25 +0100

    applied basic exception handling to card_validate

--- card/main.py
+++ card/main.py
@@ -1,41 +1,52 @@
-american = [375987654321001, 379996620002002, 377107234567890]
-american2 = [378282246310005, 371449635398431, 378734493671000]
-master = [5555555555554444, 5105105105105100]
-visa = [4111111111111111, 4012888888881881, 4222222222222, 4003600000000014]
+# american = [375987654321001, 379996620002002, 377107234567890]
+# american2 = [378282246310005, 371449635398431, 378734493671000]
+# master = [5555555555554444, 5105105105105100]
+# visa = [4111111111111111, 4012888888881881, 4222222222222, 4003600000000014]
 
 
-def validate_card(card):
+def validate_card():
+    """
+    The Validate_Card Function take a credit card number and determine whether that number is an
+    American Express Credit Card number or Master Card number or Visa or something else. 
+    The card number is provided as a string input and it has to satisfy a checksum or Luhn Algorithm
+    to be a valid credit card number.
+
+    return: AMEX | MASTERCARD | VISA | INVALID - Depending on the result of the validation 
+    """
+    print("Hello and Welcome")
+    card = input("Please enter your Card Number: ").strip()
     msg = ""
-    card_length = len(str(card))
-    if card_length == 15:
-        msg = "amex"
-    elif card_length == 16 and str(card)[0:2] in ['51', '52', '53', '54', '55']:
-        msg = "MasterCard"
-    elif (card_length == 13 or len(str(card)) == 16) and str(card)[0] == '4':
-        msg = "Visa"
-    else:
-        return "INVALID"
-
-    products = [str(card)[x] for x in range(len(str(card)) - 2, -1, -2)]
-    dbl_products = [int(x) * 2 for x in products]
-    sum_products = 0
-
-    sum_others = sum([int(str(card)[x]) for x in range(card_length - 1, -1, -2)])
-
-    for x in dbl_products:
-        if len(str(x)) > 1:
-            double = sum([int(x) for x in str(x)])
-            sum_products += double
+    try:
+        card_length = len(card)
+        if card_length == 15:
+            msg = "amex"
+        elif card_length == 16 and str(card)[0:2] in ['51', '52', '53', '54', '55']:
+            msg = "MasterCard"
+        elif (card_length == 13 or len(card) == 16) and card[0] == '4':
+            msg = "Visa"
         else:
-            sum_products += x
+            return "INVALID"
 
-    sum_total = sum_products + sum_others
-    if sum_total % 10:
-        msg = "Invalid"
+        products = [card[x] for x in range(len(card) - 2, -1, -2)]
+        dbl_products = [int(x) * 2 for x in products]
+        sum_products = 0
 
-    return msg.upper()
+        sum_others = sum([int(card[x]) for x in range(card_length - 1, -1, -2)])
+
+        for x in dbl_products:
+            if len(str(x)) > 1:
+                double = sum([int(x) for x in str(x)])
+                sum_products += double
+            else:
+                sum_products += x
+
+        sum_total = sum_products + sum_others
+        if sum_total % 10:
+            msg = "Invalid"
+    except Exception as err:
+        msg = f"An Exception Occurred: {err}"
+    finally:
+        return msg.upper()
 
 
-cards = american + american2 + master + visa
-for card in cards:
-    print(validate_card(card))
+print(validate_card())
